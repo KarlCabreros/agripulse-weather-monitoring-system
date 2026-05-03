858d3b01baa7d754e1edbf61f2896c966ad3656b
Karl Almazan <karlalmazan69@gmail.com>
2026-05-03 18:21:14 +0800

    added user managament route and audit trail route

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,9 @@ Route::view('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
-        Route::view('farm-activities', 'pages.farm-activities')->name('farm-activities');
+    Route::view('farm-activities', 'pages.farm-activities')->name('farm-activities');
+    Route::view('user-management', 'pages.user-management')->name('user-management');
+    Route::view('audit-trail', 'pages.audit-trail')->name('audit-trail');
 });
 
 require __DIR__.'/settings.php';
