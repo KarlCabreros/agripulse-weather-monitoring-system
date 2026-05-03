5322666343a564b251b65c1cf50c087241a988b0
Karl Almazan <karlalmazan69@gmail.com>
2026-05-03 18:23:49 +0800

    added user roles for creating new users

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -22,12 +22,14 @@ class CreateNewUser implements CreatesNewUsers
         Validator::make($input, [
             ...$this->profileRules(),
             'password' => $this->passwordRules(),
+            'role'=> ['required','in:owner,manager,worker'],
         ])->validate();
 
         return User::create([
             'name' => $input['name'],
             'email' => $input['email'],
             'password' => $input['password'],
+            'role' =>$input['role'],
         ]);
     }
 }
